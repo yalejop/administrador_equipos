497402568a77c67860c9e97a52737e5b3e429de9
Alejandro Cataño Herrera <alejo.9211@hotmail.com>
2022-11-06 15:52:19 -0500

    proyecto administrador de equipos

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+import ManagePlayerPage from "./components/pages/ManagePlayerPage/ManagePlayerPage";
+import NewPlayerPage from "./components/pages/NewPlayerPage/NewPlayerPage";
+import WelcomePage from "./components/pages/WelcomePages/WelcomePage";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+
+      <BrowserRouter>
+
+        <Switch>
+          <Route path="/players/list" exact render={() => <WelcomePage />} />
+          <Route path="/players/addplayer" exact render={() => <NewPlayerPage />} />
+          <Route path="/status/game/:id" exact render={(routeProps) => <ManagePlayerPage {...routeProps} />} />
+        </Switch>
+
+      </BrowserRouter>
+
     </div>
   );
 }
